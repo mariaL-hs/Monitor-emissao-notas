9a279ec97df463d0c1248f5e096a82d0021a5ea7
Maria Luiza <marialuizahames8@gmail.com>
2026-06-26 19:59:16 -0300

    correcao main.java

---
+++ model/StatusNota.java
@@ -0,0 +1,10 @@
+// objetos que representam os dados do sistema
+package model;
+
+public enum StatusNota {
+    PENDENTE_AUTORIZACAO,
+    AUTORIZADA,
+    REJEICAO_SEFAZ,
+    INUTILIZADA,
+    CANCELADA
+}
---
+++ service/NotaFiscalService.java
@@ -0,0 +1,81 @@
+// regras de negócio
+
+package service;
+
+import java.time.LocalDate;
+import model.NotaFiscal;
+import model.StatusNota;
+
+// recebe uma nota fiscal e retorna true/false
+public class NotaFiscalService {
+    public boolean inutilizarNota(NotaFiscal nota) {
+    if (nota.getStatus() == StatusNota.REJEICAO_SEFAZ) {
+        nota.setStatus(StatusNota.INUTILIZADA);
+        return true;
+    }
+
+    else if (nota.getStatus() == StatusNota.INUTILIZADA) {
+        System.out.println("A nota já está inutilizada");
+        return false;
+    }
+
+    else {
+        System.out.println("A nota não está rejeitada pela SEFAZ");
+        return false;
+        }
+    }
+
+    public boolean cancelarNota(NotaFiscal nota) {
+        if (nota.getStatus() == StatusNota.AUTORIZADA) {
+            if (nota.getDataEmissao()
+                .plusDays(30)
+                .isBefore(LocalDate.now())) {
+                System.out.println("Fora do prazo de cancelamento.");
+                return false; 
+                }
+
+            nota.setStatus(StatusNota.CANCELADA);
+            return true;
+        }
+
+        else if (nota.getStatus() == StatusNota.CANCELADA) {
+            System.out.println("A nota já está cancelada");
+            return false;
+        
+        }
+        
+        else {
+        System.out.println("A nota não está autorizada pela SEFAZ e não pode ser cancelada");
+        return false;
+        }
+    }
+
+    public boolean autorizarNota(NotaFiscal nota) {
+
+        if (nota.getStatus() == StatusNota.PENDENTE_AUTORIZACAO) {
+            nota.setStatus(StatusNota.AUTORIZADA);
+            System.out.println("Nota autorizada com sucesso.");
+            return true;
+        }
+
+        else if (nota.getStatus() == StatusNota.AUTORIZADA) {
+            System.out.println("A nota já está autorizada.");
+            return false;
+        }
+
+        else if (nota.getStatus() == StatusNota.CANCELADA) {
+            System.out.println("Não é possível autorizar uma nota cancelada.");
+            return false;
+        }
+
+        else if (nota.getStatus() == StatusNota.INUTILIZADA) {
+            System.out.println("Não é possível autorizar uma nota inutilizada.");
+            return false;
+        }
+
+        else {
+            System.out.println("A nota não pode ser autorizada.");
+            return false;
+        }
+    }
+}
